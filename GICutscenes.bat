730e61b665edbaf24165185890be74117f728424
Stanislav Ferrao <64654942+ft-scobra@users.noreply.github.com>
2024-02-26 17:24:50 -0800

    Update GICutscenes.bat
    Removed `-s`. 
    This one will work without needing to download anything.

--- GICutscenes.bat
+++ GICutscenes.bat
@@ -2,4 +2,4 @@
 echo Drag and drop your USM file here, or enter the path manually:
 set /p file="Enter the path to the file: "
 set "path=%file:"=%"
-GICutscenes demuxUsm "%path%" -m -s
+GICutscenes demuxUsm "%path%" -m
